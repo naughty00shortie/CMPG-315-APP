ab71b4f0f9a034796ff660811183daf66aa0d6aa
naughty00shortie <naughty00shortie@gmail.com>
2023-04-28 10:41:57 +0200

    init
    The IP address to the server need to be changed for this to work on your side.

---
+++ Client/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                IPAddress serverIpAddress = IPAddress.Parse("10.0.0.20"); // IP address of the server
+                int port = 20; // port number that the server is listening on
+
+                using Socket server = new(
+                    serverIpAddress.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp);
+
+
+                await server.ConnectAsync(serverIpAddress, port);
+                Console.WriteLine("Connected to server: {0}", server.RemoteEndPoint);
+
+                byte[] buffer = new byte[1024];
+                while (true)
+                {
+                   
+                    int numBytes = await server.ReceiveAsync(buffer, SocketFlags.None);
+                    string chatLog = Encoding.UTF8.GetString(buffer, 0, numBytes);
+                
+                    Console.WriteLine(chatLog);
+                
+                    // read messages from the console and send them to the server
+                    while (true)
+                    {
+                        Console.Write("Enter message: ");
+                        string message = Console.ReadLine();
+                        byte[] data = Encoding.UTF8.GetBytes(message);
+                        await server.SendAsync(data, SocketFlags.None);
+                        
+                        Task.Run(() => RecieveMessages(server));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not connect to server");
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        static async Task RecieveMessages(Socket server)
+        {
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                int numBytes = await server.ReceiveAsync(buffer, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(buffer, 0, numBytes);
+                Console.WriteLine("Received message: " + message);
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Any; // listen on any available IP address
+            int port = 20; // port number to listen on
+
+            IPEndPoint ipEndPoint = new(ipAddress, port);
+
+            using Socket listener = new(
+                ipEndPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp);
+
+            listener.Bind(ipEndPoint);
+            listener.Listen(1);
+
+            Console.WriteLine("Server started. Listening for connections...");
+            List<Socket> connectedClients = new List<Socket>();
+            while (true)
+            { 
+                try
+                {
+                    Socket client = await listener.AcceptAsync();
+                    connectedClients.Add(client);
+                    Console.WriteLine("Client connected: {0}", client.RemoteEndPoint);
+
+                    IPEndPoint remoteIpEndPoint = (IPEndPoint)client.RemoteEndPoint;
+                    IPAddress clientIpAddress = remoteIpEndPoint.Address;
+                    Console.WriteLine("Client IP address: {0}", clientIpAddress);
+
+                    
+                    string chatLog = File.ReadAllText("chatlog.txt");
+                    byte[] chatLogBytes = Encoding.UTF8.GetBytes(chatLog);
+                    await client.SendAsync(chatLogBytes, SocketFlags.None);
+
+                    Task.Run(() => ReceiveMessages(client, connectedClients));
+                        
+                    
+                }
+                
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+
+        static async Task ReceiveMessages(Socket client, List<Socket> connectedClients)
+        {
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                int numBytes = await client.ReceiveAsync(buffer, SocketFlags.None);
+                string message = Encoding.UTF8.GetString(buffer, 0, numBytes);
+                Console.WriteLine("Received message: " + message);
+                string chatLog = File.ReadAllText("chatlog.txt");
+                chatLog += message + "\n";
+                File.WriteAllText("chatlog.txt", chatLog);
+                byte[] chatLogBytes = Encoding.UTF8.GetBytes(chatLog);
+                foreach (Socket connectedClient in connectedClients)
+                { 
+                    if(connectedClient != client)
+                        await connectedClient.SendAsync(chatLogBytes, SocketFlags.None);
+                }
+            }
+        }
+    }
+}
